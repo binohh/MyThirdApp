9997144f9978acff927602463352e53b1cb29cdc
binohh <vuathegioi@gmail.com>
2018-12-25 13:59:29 +0700

    Initial commit ahihi

---
+++ app/src/main/java/com/example/mythirdapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mythirdapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        // Hiiiiii xin chao cac ban
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
